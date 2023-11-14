6482a3f10d5ebd3762fb1e04036a8c1a4f75acf0
Oleh <dwemov@gmail.com>
2023-11-14 20:51:24 +0200

    11

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //byte b1=0;
+        System.out.println("Hello and ");
+        System.out.print("welcome!");
+
+    }
+}
